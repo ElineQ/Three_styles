278eb98e5d296fb0639ed72b578e0c4d4df51f6f
ElineQ <84079851+ElineQ@users.noreply.github.com>
2021-06-19 14:48:42 +0200

    Add files via upload

---
+++ Three_styles.js
@@ -0,0 +1,28 @@
+
+let y = 5 // y contains number 5
+let x = 4 // x contains number 4
+
+//function declaration
+function MultiplyNumber1(x) { return x * x; };
+console.log(MultiplyNumber1);
+
+
+function MultiplyNumber2(y) { return y * y };
+console.log(MultiplyNumber2);
+
+//function expression
+const MultiplyNumber3 = function (x) { return x * x; };
+console.log(MultiplyNumber3);
+
+
+const MultiplyNumber4 = function (y) { return y * y };
+console.log(MultiplyNumber4);
+
+
+//Arrow function
+const MultiplyNumber5 = (x) * { return x * x; };
+console.log(MultiplyNumber5);
+
+const MultiplyNumber6 = (y) * { return y * y; };
+console.log(MultiplyNumber6);
+
